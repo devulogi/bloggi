2ca5ccf23c86c3148b6bc53fc51135083ee0dafb
devulogi <robertdpalmares@gmail.com>
2020-10-26 22:18:46 +0800

    helper created

---
+++ config/helper.js
@@ -0,0 +1,17 @@
+module.exports = {
+  checkAuthentication: function (req, res, next) {
+    if (req.isAuthenticated()) {
+      next();
+    } else {
+      req.flash("info", "You must be logged in to comment.");
+      res.redirect("/");
+    }
+  },
+  routerMiddleware: function (req, res, next) {
+    res.locals.currentUser = req.user;
+    res.locals.errors = req.flash("error");
+    res.locals.infos = req.flash("info");
+    res.locals.sucessess = req.flash("success");
+    next();
+  },
+};
