e4d1bebb5c8c46c24bc4d468ea67dccca8174afa
devulogi <robertdpalmares@gmail.com>
2020-11-05 01:52:32 +0800

    refactored

--- index.js
+++ index.js
@@ -2,19 +2,26 @@ const express = require("express");
 const path = require("path");
 require("dotenv").config();
 
+// express instance
 const app = express();
 
+// template engine
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
+// relative file path
 app.use(express.static(path.join(__dirname, "public")));
 
+// runs every request
 require("./config/middlewares")(app);
 
+// routes
 app.use(require("./routes/gen.route"));
 
+// mongo database connection
 require("./config/mongodb")();
 
+// listens to http traffic
 app.listen(process.env.PORT || 3000, function () {
   console.log("Application running...");
 });
