ce2001b84e98c7ae86595346eb6e13e7b85f50da
devulogi <robertdpalmares@gmail.com>
2020-11-06 00:34:08 +0800

    added useFindAndModify: false,

--- config/mongodb.js
+++ config/mongodb.js
@@ -4,6 +4,7 @@ const DbConnection = function () {
   mongoose.connect("mongodb://localhost:27017/bloggi", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useFindAndModify: false,
   });
 };
 
